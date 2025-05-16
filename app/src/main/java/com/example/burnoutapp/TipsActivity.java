618a323401acac901cb76c37bcffb5ef86fda02e
DelMiiYa <siriyaaaaa36@gmail.com>
2025-05-16 16:38:59 +0700

    Changed buttonBack to use an icon and navigate back to the menu.

--- app/src/main/java/com/example/burnoutapp/TipsActivity.java
+++ app/src/main/java/com/example/burnoutapp/TipsActivity.java
@@ -2,6 +2,7 @@ package com.example.burnoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -52,10 +53,9 @@ public class TipsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // go go menu
-                //Intent intent = new Intent(TipsActivity.this, MenuActivity.class);
-                //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // clear back stack
-                //startActivity(intent);
-                finish();
+                Intent intent = new Intent(TipsActivity.this, MenuActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // clear back stack
+                startActivity(intent);
             }
         });
     }
